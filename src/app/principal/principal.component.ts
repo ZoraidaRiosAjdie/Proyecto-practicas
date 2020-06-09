bd62fcb1b07a1725ab9a8099e39824b1a6c2ec77
ZoraidaRiosAjdie <zoraidariosajdie@gmail.com>
2020-06-09 13:28:36 +0200

    despliegue

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -18,6 +18,7 @@ export interface Farm {
   //encapsulation: ViewEncapsulation.None
 })
 export class PrincipalComponent {
+    public boleano : Boolean = true;
     constructor(private _peticionService:PeticionService){
 
     }
@@ -50,8 +51,8 @@ export class PrincipalComponent {
             };
             let boton : Object = {
                 MasinfoB : {type: 'HTML',  
-                            content:'<input type="button" value="+"><br><input type="button" value="-">'}, style: { fill : 'rgb(255, 184, 215)'}
-                // Principal : [{id:"Name", height: 00, width: 60, offset: 0.5, template: '<h1>F<br>A<br>R<br>M</h1>' }]
+                            content:'<input type="button" value="+" (click)="handleClick(true)"><br><input type="button" value="-" (click)="handleClick(false)">'}, 
+                            style: { fill : 'rgb(255, 184, 215)'}
             };
             let margin : Object = {
                 Masinfo : { top: 10, left: 10, right: 10, bottom: 10 },
@@ -65,15 +66,26 @@ export class PrincipalComponent {
             }
             let height :Object = {
                 MasinfoB : 80,
-            }
+            } 
+            /*if (this.boleano == true) {
+                nodeModel.annotations = [{
+                    content: data['Name'], margin : margin[(nodeModel.data as Farm).Role], 
+                    style: { color: 'black' ,
+                             fill: codes[(nodeModel.data as Farm).Role] },
+                    template: boton[(nodeModel.data as Farm).Role]
+                }];
+                nodeModel.style = { fill : codes[(nodeModel.data as Farm).Role] , strokeColor: '#f5d897', strokeWidth: 1};
+                // nodeModel.shape =  boton[(nodeModel.data as Farm).Role];
+            }*/
             nodeModel.annotations = [{
                 content: data['Name'], margin : margin[(nodeModel.data as Farm).Role], 
                 style: { color: 'black' ,
-                        fill: codes[(nodeModel.data as Farm).Role] },
+                         fill: codes[(nodeModel.data as Farm).Role] }
                 //template: boton[(nodeModel.data as Farm).Role]
             }];
-            nodeModel.style = { fill: codes[(nodeModel.data as Farm).Role] , strokeColor: '#f5d897', strokeWidth: 1};
-            nodeModel.shape =  boton[(nodeModel.data as Farm).Role]
+            nodeModel.style = { fill : codes[(nodeModel.data as Farm).Role] , strokeColor: '#f5d897', strokeWidth: 1};
+            nodeModel.shape =  boton[(nodeModel.data as Farm).Role];
+
             // nodeModel.rotateAngle = rota[(nodeModel.data as Farm).Role];
             // nodeModel.annotations = boton[(nodeModel.data as Farm).Role];
         }
@@ -95,63 +107,11 @@ export class PrincipalComponent {
             verticalAlignment: 'Top',
             orientation: 'LeftToRight'
     };
-    /*@ViewChild("diagram")
-    public diagram: DiagramComponent;
-    public offset: PointModel
-    public verticalAlignment: VerticalAlignment
-    public getTextElement(text: string): TextElement {
-        let textElement: TextElement = new TextElement();
-        textElement.width = 50;
-        textElement.height = 20;
-        textElement.content = text;
-        return textElement;
-    };
-
-    public addRows(column: StackPanel) {
-        column.children.push(this.getTextElement('Row1'));
-        column.children.push(this.getTextElement('Row2'));
-        column.children.push(this.getTextElement('Row3'));
-        column.children.push(this.getTextElement('Row4'));
-    };
-    public getNodeDefaults(node: NodeModel): NodeModel {
-        node.height = 100;
-        node.width = 100;
-        node.style.fill = '#6BA5D7';
-        node.style.strokeColor = 'white';
-        return node;
+    handleClick(valor: Boolean) {
+        this.boleano = valor;
+        console.log(valor);
     }
-    ngOnInit(): void {
-        this.verticalAlignment = 'Top';
-        this.offset = {y: 1};
-        this.diagram.setNodeTemplate = (obj: NodeModel, diagram: Diagram): StackPanel => {
-            if (obj.id.indexOf('node1') !== -1) {
-                // It will be replaced with grid panel
-                let table: StackPanel = new StackPanel();
-                table.orientation = 'Horizontal';
-                let column1: StackPanel = new StackPanel();
-                column1.children = [];
-                column1.children.push(this.getTextElement('Column1'));
-                this.addRows(column1);
-                let column2: StackPanel = new StackPanel();
-                column2.children = [];
-                column2.children.push(this.getTextElement('Column2'));
-                this.addRows(column2);
-                table.children = [column1, column2];
-                return table;
-            }
-            return null
-        }
-    }*/
 }
-  
-/*export class Default{    
-    rendereComplete() {  
-      //click event for html content  
-      document.getElementById("input").onclick = function () {  
-        alert("button")  
-      }  
-    }  
-}*/
 export interface DataInfo {
     [key: string]: string;
 }
